119ee28ccc934ea3ab659a96b8864c6ecdeb1eb2
Adem Kaya <adeemkaya16@gmail.com>
2024-09-25 20:03:07 +0000

    Finish logging

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,24 @@
+import logging
 from django.utils import timezone
 from blog.models import Post
 from django.shortcuts import render, get_object_or_404, redirect
 from blog.forms import CommentForm
 
+# Get the logger for this module
+logger = logging.getLogger(__name__)
+
 # Create your views here.
 def index(request):
+    # Retrieve posts from the database
     posts = Post.objects.filter(published_at__lte=timezone.now())
+    
+    # Log the number of posts retrieved at DEBUG level
+    logger.debug("Got %d posts", len(posts))
+    
     return render(request, "blog/index.html", {"posts": posts})
 
 def post_detail(request, slug):
+    # Fetch the post by its slug
     post = get_object_or_404(Post, slug=slug)
 
     if request.user.is_active:
@@ -16,10 +26,17 @@ def post_detail(request, slug):
             comment_form = CommentForm(request.POST)
 
             if comment_form.is_valid():
+                # Save the comment
                 comment = comment_form.save(commit=False)
                 comment.content_object = post
                 comment.creator = request.user
                 comment.save()
+
+                # Log a message after the comment is saved
+                logger.info(
+                    "Created comment on Post %d for user %s", post.pk, request.user
+                )
+
                 return redirect(request.path_info)
         else:
             comment_form = CommentForm()
