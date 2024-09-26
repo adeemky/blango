2bf7521f85ac602ccc0de3ff34b43a4da486d67e
Adem Kaya <adeemkaya16@gmail.com>
2024-09-26 14:44:44 +0000

    Finish custom User model

--- blango/settings.py
+++ blango/settings.py
@@ -52,7 +52,8 @@ class Dev(Configuration):
       'blog',
       'crispy_forms',
       'crispy_bootstrap5',
-      "debug_toolbar",
+      'debug_toolbar',
+      'blango_auth',
   ]
 
   MIDDLEWARE = [
@@ -65,6 +66,8 @@ class Dev(Configuration):
       'django.contrib.messages.middleware.MessageMiddleware',
   #     'django.middleware.clickjacking.XFrameOptionsMiddleware',
   ]
+
+  AUTH_USER_MODEL = "blango_auth.User"
 
   INTERNAL_IPS = ["192.168.10.226"]
 
