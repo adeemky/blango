312370c4e9d0921f1704504c6e00e1b0e478f841
Adem Kaya <adeemkaya16@gmail.com>
2024-09-30 00:38:19 +0000

    Finish permissions

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -5,6 +5,8 @@ from rest_framework.authtoken import views
 from blog.api.views import PostList, PostDetail
 
 urlpatterns = [
+    path("posts/", PostList.as_view()),
+    path("posts/<int:pk>/", PostDetail.as_view()),
     path("auth/", include("rest_framework.urls")),
     path("token-auth/", views.obtain_auth_token)
 ]
