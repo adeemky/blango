03093bbc34d6992e5e9e31d189ed7d984e2d45db
Adem Kaya <adeemkaya16@gmail.com>
2024-10-02 23:53:07 +0000

    Finish simplejwt

--- blango/settings.py
+++ blango/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 from configurations import Configuration
 from configurations import values
 import dj_database_url
+from datetime import timedelta
 
 class Dev(Configuration):
 
@@ -180,6 +181,7 @@ class Dev(Configuration):
       "rest_framework.authentication.BasicAuthentication",
       "rest_framework.authentication.SessionAuthentication",
       "rest_framework.authentication.TokenAuthentication",
+      "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticatedOrReadOnly"
@@ -203,6 +205,11 @@ class Dev(Configuration):
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 100,
   }
+
+  SIMPLE_JWT = {
+        "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+        "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+    }
 
   SWAGGER_SETTINGS = {
         "SECURITY_DEFINITIONS": {
